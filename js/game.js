26d6fd862a3f9ff423256f6de1f70acbffb6247e
TheGamerdk <carlgustav03@gmail.com>
2017-06-30 17:48:38 +0200

    Javascript visual finish

--- js/game.js
+++ js/game.js
@@ -4,9 +4,15 @@ function employPress(company) {
 	document.getElementById(company).onclick = "firePress(company);";
 	document.getElementById(company).innerHTML = "FIRE";
 	document.getElementById(company).className = "btn btn-lg btn-danger";
+	document.getElementById(company + "_Employ").innerHTML = "EMPLOYED";
+	document.getElementById(company + "_Employ").style = "color: green;";
 }
 
 function firePress(company) {
 	console.log(company);
-
+	document.getElementById(company).onclick = "employPress(company);";
+	document.getElementById(company).innerHTML = "EMPLOY";
+	document.getElementById(company).className = "btn btn-lg btn-primary";
+	document.getElementById(company + "_Employ").innerHTML = "NOT EMPLOYED";
+	document.getElementById(company + "_Employ").style = "color: red;";
 }
