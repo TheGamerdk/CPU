c8b17cc3faedff88184e27f8414d21d79e573778
TheGamerdk <carlgustav03@gmail.com>
2017-06-30 17:35:07 +0200

    Button Usage

--- js/game.js
+++ js/game.js
@@ -1,4 +1,11 @@
 
 function employPress(company) {
 	console.log(company);
+	document.getElementById(company).onclick = firePress(company);
+	document.getElementById(company).value = "FIRE";
+	document.getElementById(company).style = "btn btn-lg btn-danger";
+}
+
+function firePress(company) {
+	console.log(company);
 }
